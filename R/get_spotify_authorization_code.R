de0beb5a3decf91811dbbc07dd732ed4d1049094
skurp <wconnell93@gmail.com>
2018-08-01 15:32:35 -0700

    updated user params

--- R/get_spotify_authorization_code.R
+++ R/get_spotify_authorization_code.R
@@ -14,5 +14,5 @@
 get_spotify_authorization_code <- function(client_id = Sys.getenv("SPOTIFY_CLIENT_ID"), client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"), scope = 'user-library-read user-library-modify playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative user-read-recently-played user-top-read user-read-private user-read-email user-read-birthdate streaming user-modify-playback-state user-read-currently-playing user-read-playback-state user-follow-modify user-follow-read') {
     endpoint <- oauth_endpoint(authorize = 'https://accounts.spotify.com/authorize', access = 'https://accounts.spotify.com/api/token')
     app <- oauth_app('spotifyr', client_id, client_secret)
-    oauth2.0_token(endpoint = endpoint, app = app, scope = scope)
+    oauth2.0_token(endpoint = endpoint, app = app, scope = scope, user_params = list(response_type = "code"))
 }
